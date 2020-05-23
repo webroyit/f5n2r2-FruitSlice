ced2e90b339ee1848449e4f6e3d71fea3a14efdb
royweb <xytank@gmail.com>
2020-05-23 12:07:58 -0400

    Check for the mouse clicked input

--- Assets/Scripts/Blade.cs
+++ Assets/Scripts/Blade.cs
@@ -5,6 +5,25 @@ public class Blade : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Listen for press down on the left click of the mouse
+        if(Input.GetMouseButtonDown(0))
+        {
+            StartCutting();
+        } 
+        // Listen for release of the left click of the mouse
+        else if(Input.GetMouseButtonUp(0))
+        {
+            StopCutting();
+        }
+    }
+
+    void StartCutting()
+    {
+
+    }
+
+    void StopCutting()
+    {
         
     }
 }
